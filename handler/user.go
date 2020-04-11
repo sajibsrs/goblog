600b7337e967ad38f14ca168909015f4be4c1b71
Sajidur Rahman <sajibsrs@gmail.com>
2020-04-11 14:01:45 +0600

    user templates added

---
+++ handler/user.go
@@ -0,0 +1,19 @@
+// This file handles user routes of the application
+// and serves templates accordingly
+
+package handler
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func User(w http.ResponseWriter, r *http.Request) {
+	files := []string{
+		"template/default/layout.html",
+		"template/user/detail.html",
+		"template/default/navigation.html"}
+
+	tmpl := template.Must(template.ParseFiles(files...))
+	_ = tmpl.ExecuteTemplate(w, "layout", r)
+}
--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func main() {
 	files := http.FileServer(http.Dir("static"))
 	mux.Handle("/static/", http.StripPrefix("/static/", files))
 	mux.HandleFunc("/", handler.Index)
+	mux.HandleFunc("/users/", handler.User)
 	server := &http.Server{
 		Addr:              "127.0.0.1:2000",
 		Handler:           mux,
