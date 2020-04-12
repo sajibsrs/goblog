91d5584080e3e71dd49252e89523281dd470e6e2
Sajidur Rahman <sajibsrs@gmail.com>
2020-04-12 14:34:00 +0600

    code refactor

--- data/data.go
+++ data/data.go
@@ -16,7 +16,7 @@ func init() {
 	fmt.Println("Database connected")
 }
 
-func generateUUID() string {
+func generateUUID() (uuid string) {
 	u := new([16]byte)
 	_, err := rand.Read(u[:])
 	if err != nil {
@@ -24,5 +24,6 @@ func generateUUID() string {
 	}
 	u[8] = (u[8] | 0x40) & 0x7F
 	u[6] = (u[6] & 0xF) | (0x4 << 4)
-	return fmt.Sprintf("%x-%x-%x-%x-%x", u[0:4], u[4:6], u[6:8], u[8:10], u[10:])
+	uuid = fmt.Sprintf("%x-%x-%x-%x-%x", u[0:4], u[4:6], u[6:8], u[8:10], u[10:])
+	return
 }
