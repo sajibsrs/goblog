8a4df11c9baa125e1ed62314ae54406e42daada2
Sajidur Rahman <sajibsrs@gmail.com>
2020-04-13 07:41:44 +0600

    bug fixed, code refactored, GenerateUUID method added

--- data/data.go
+++ data/data.go
@@ -2,28 +2,42 @@ package data
 
 import (
 	"crypto/rand"
+	"crypto/sha1"
 	"database/sql"
 	"fmt"
+	"log"
+
+	// Import mysql driver
 	_ "github.com/go-sql-driver/mysql"
 )
 
+// DB define database variable
+var DB *sql.DB
+
 func init() {
-	db, err := sql.Open("mysql", "root:8080k@tcp(localhost)/goblog")
+	var err error
+	DB, err = sql.Open("mysql", "root:8080k@tcp(localhost)/goblog")
 	if err != nil {
 		panic(err.Error())
 	}
-	defer db.Close()
-	fmt.Println("Database connected")
+	return
 }
 
-func generateUUID() (uuid string) {
+// GenerateUUID generates a new uuid
+func GenerateUUID() (uuid string) {
 	u := new([16]byte)
 	_, err := rand.Read(u[:])
 	if err != nil {
-		panic(err.Error())
+		log.Fatalln("Cannot generate UUID", err)
 	}
 	u[8] = (u[8] | 0x40) & 0x7F
 	u[6] = (u[6] & 0xF) | (0x4 << 4)
 	uuid = fmt.Sprintf("%x-%x-%x-%x-%x", u[0:4], u[4:6], u[6:8], u[8:10], u[10:])
+	return
+}
+
+// Encrypt function encrypts string with SHA-1
+func Encrypt(str string) (crypt string) {
+	crypt = fmt.Sprintf("%x", sha1.Sum([]byte(str)))
 	return
 }
