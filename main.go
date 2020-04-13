9e0dde5ffdafb837087a9bb62830a6842a7d8e7c
Sajidur Rahman <sajibsrs@gmail.com>
2020-04-13 17:26:24 +0600

    code organization

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ package main
 
 import (
 	"goblog/handler"
+	"goblog/handler/user"
 	"net/http"
 )
 
@@ -13,8 +14,8 @@ func main() {
 	files := http.FileServer(http.Dir("static"))
 	mux.Handle("/static/", http.StripPrefix("/static/", files))
 	mux.HandleFunc("/", handler.Index)
-	mux.HandleFunc("/signup/", handler.CreateUser)
-	mux.HandleFunc("/users/", handler.User)
+	mux.HandleFunc("/signup/", user.Create)
+	mux.HandleFunc("/users/", user.Index)
 	server := &http.Server{
 		Addr:    "127.0.0.1:2000",
 		Handler: mux,
