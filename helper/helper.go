810d5ae7cc94c2902282281997026b9e4db57bc4
Sajidur Rahman <sajibsrs@gmail.com>
2020-04-15 13:05:12 +0600

    added helper function to handle templates

---
+++ helper/helper.go
@@ -0,0 +1,29 @@
+// This file handles helper and utility functions of the application
+
+package helper
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"strings"
+)
+
+// ProcessTemplates processes, parses and executes template files
+//  w = response writer
+//  name = template name
+//  data = data to be passed to the template
+//  tmp = template names to process in "directory.templateName" format
+// Default template directory is named template and extension should be omitted
+func ProcessTemplates(w http.ResponseWriter, name string, data interface{} , tmp ...string) {
+	var procTmp []string
+	for _, t := range tmp {
+		t = "template/" + strings.ReplaceAll(t, ".", "/") + ".html"
+		procTmp = append(procTmp, t)
+	}
+	temps := template.Must(template.ParseFiles(procTmp...))
+	err := temps.ExecuteTemplate(w, name, data)
+	if err != nil {
+		log.Println("Unable to execute templates", err)
+	}
+}
