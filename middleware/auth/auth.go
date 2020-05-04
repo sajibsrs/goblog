dbc84802c903b8386c0c343958913f723d6aa613
Sajidur Rahman <sajibsrs@gmail.com>
2020-05-04 18:07:13 +0600

    added session management methods

--- middleware/auth/auth.go
+++ middleware/auth/auth.go
@@ -1,9 +1,9 @@
 package auth
 
 import (
-	"errors"
 	"github.com/julienschmidt/httprouter"
 	"goblog/database"
+	"goblog/helper"
 	"goblog/model"
 	"log"
 	"net/http"
@@ -17,7 +17,6 @@ func CreateSession(w http.ResponseWriter, r *http.Request,  _ httprouter.Params)
 		log.Println("Unable to parse form", err)
 		return
 	}
-	expiration := time.Now().Add(24 * time.Hour)
 	usr, _ := model.GetUserByEmail(r.PostFormValue("email"))
 	if usr.Password == database.Encrypt(r.PostFormValue("password")) {
 		sessionID, _ := usr.CreateSession()
@@ -26,7 +25,7 @@ func CreateSession(w http.ResponseWriter, r *http.Request,  _ httprouter.Params)
 			Name: "goblog_session_cookie",
 			Value: session.UUID,
 			HttpOnly: true,
-			Expires: expiration,
+			Expires: time.Now().Add(24 * time.Hour),
 		}
 		http.SetCookie(w, &cookie)
 		http.Redirect(w, r, "/", 302)
@@ -35,13 +34,77 @@ func CreateSession(w http.ResponseWriter, r *http.Request,  _ httprouter.Params)
 	}
 }
 
+func CleanupSession(){
+	result, err := database.DB.Query(`SELECT * FROM sessions`)
+	if err != nil {
+		log.Println("Get all session failed", err)
+	}
+	for result.Next() {
+		var session Session
+		err = result.Scan(&session.ID, &session.UUID, &session.FName, &session.LName, &session.Email, &session.UserID,
+			&session.Created)
+		if err != nil {
+			log.Println("Unable to initialize sessions", err)
+		} else {
+			timeNow := time.Now()
+			sessionTime := session.Created
+			diff := timeNow.Sub(sessionTime).Hours()
+			if diff > 24 {
+				stmt, _ := database.DB.Prepare(`DELETE FROM sessions WHERE id = ?`)
+				_, err := stmt.Exec(session.ID)
+				if err != nil {
+					log.Println("Unable to remove session", err)
+				}
+			}
+		}
+	}
+}
+
+// ValidateSession gets existing cookie
+// and checks if that session exists
 func ValidateSession(w http.ResponseWriter, r *http.Request) (session Session, err error) {
 	cookie, err := r.Cookie("goblog_session_cookie")
 	if err == nil {
 		session = Session{UUID: cookie.Value}
 		if valid, _ := session.Validate(); !valid {
-			err = errors.New("invalid session")
+			cookie := http.Cookie{
+				Name: "goblog_session_cookie",
+				Value: session.UUID,
+				HttpOnly: true,
+				Expires: time.Now(),
+				MaxAge: -1,
+			}
+			http.SetCookie(w, &cookie)
+			log.Println("Invalid session", err)
+			http.Redirect(w, r, "/login", 302)
 		}
+	} else {
+		log.Println("Session doesn't exists", err)
+		http.Redirect(w, r, "/login", 302)
 	}
 	return
+}
+
+// GetSessionByID returns a session based on given id
+func GetSessionByID(id int64) (session Session, err error) {
+	err = database.DB.QueryRow(`SELECT id, uuid, fname, lname, email, usr_id, created_at FROM sessions 
+		WHERE id = ?`, id).Scan(&session.ID, &session.UUID, &session.FName, &session.LName, &session.Email,
+		&session.UserID, &session.Created)
+	if err != nil {
+		log.Println("Get session by id query failed", err)
+		return
+	}else {
+		log.Println("Session retrieved by id successfully")
+	}
+	return
+}
+
+// Login handles existing user login
+func Login(w http.ResponseWriter, r *http.Request, _ httprouter.Params)  {
+	tmp := []string{
+		"user.login",
+		"default.layout",
+		"default.navigation",
+	}
+	helper.ProcessTemplates(w, "layout", nil, tmp...)
 }
