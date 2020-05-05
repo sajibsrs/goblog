9d3fa18b083b3794cad585b7a61516daec416790
Sajidur Rahman <sajibsrs@gmail.com>
2020-05-05 18:07:58 +0600

    query update

--- middleware/auth/session.go
+++ middleware/auth/session.go
@@ -1,8 +1,7 @@
-package model
+package auth
 
 import (
 	"goblog/database"
-	"log"
 	"time"
 )
 
@@ -19,27 +18,15 @@ type Session struct {
 
 // Validate validates a session against given session UUID
 func (session *Session) Validate() (valid bool, err error) {
-	err = database.DB.QueryRow("SELECT id, uuid, fname, lname, email, usr_id, created_at FROM sessions WHERE uuid = ?", session.UUID).
-		Scan(&session.ID, &session.UUID, &session.FName, &session.LName, &session.Email, &session.UserID, &session.Created)
+	err = database.DB.QueryRow(`SELECT id, uuid, fname, lname, email, usr_id, created_at FROM sessions 
+		WHERE uuid = ?`, session.UUID).Scan(&session.ID, &session.UUID, &session.FName, &session.LName,
+		&session.Email, &session.UserID, &session.Created)
 	if err != nil {
 		valid = false
 		return
 	}
 	if session.ID != 0 {
 		valid = true
-	}
-	return
-}
-
-// GetSessionByID returns a session based on given id
-func GetSessionByID(id int64) (session Session, err error) {
-	err = database.DB.QueryRow("SELECT id, uuid, fname, lname, email, usr_id, created_at FROM sessions WHERE id = ?", id).
-		Scan(&session.ID, &session.UUID, &session.FName, &session.LName, &session.Email, &session.UserID, &session.Created)
-	if err != nil {
-		log.Println("Get session by id query failed", err)
-		return
-	}else {
-		log.Println("Session retrieved by id successfully")
 	}
 	return
 }
